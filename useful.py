816cea49ce135b3e5b0fbce6f4659399f59fc617
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2023-09-27 18:16:43 +0300

    Update useful.py

--- useful.py
+++ useful.py
@@ -1,6 +1,9 @@
 "string to letters"
 string = "Geeksforgeeks"
-letter = [x for x in string
+letter = [x for x in string]
+
+def split(word):
+    return list(word)
          
 string = "geeks"
 print([*string])

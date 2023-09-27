198a37f2d1e7ad4617034cae9bb9770c4ebb7cdb
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2023-09-27 22:59:05 +0300

    Update useful.py

--- useful.py
+++ useful.py
@@ -9,8 +9,18 @@ string = "geeks"
 print([*string])
 
 #"length of string"
- a=len(nums) #nums is a List
+a=len(nums) #nums is a List
 
 #Check whether given Key already exists in a Python Dictionary
 #if key in dic.keys():
-if key in dic:
+#if key in dic:
+
+#FROM LIST TO DICTIONARY
+    keys = ['apple', 'banana', 'orange']
+values = [3, 6, 4]
+
+# zip the two lists together to create a list of key-value pairs
+key_value_pairs = zip(keys, values)
+
+# convert the list of key-value pairs to a dictionary
+my_dict = dict(key_value_pairs)

4b6a6ad3b018008e7df6d339926747a66313a5a6
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2023-09-27 22:54:59 +0300

    Update useful.py

--- useful.py
+++ useful.py
@@ -1,11 +1,16 @@
 #"string to letters"
 string = "Geeksforgeeks"
-letter = [x for x in string]
+letter = [x for x in string] 
 
 def split(word):
     return list(word)
          
 string = "geeks"
 print([*string])
+
 #"length of string"
  a=len(nums) #nums is a List
+
+#Check whether given Key already exists in a Python Dictionary
+#if key in dic.keys():
+if key in dic:

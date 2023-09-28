d1f05924ad44494c67236a2e084e09fa93fa5906
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2023-09-28 20:05:22 +0300

    Update useful.py

--- useful.py
+++ useful.py
@@ -1,3 +1,4 @@
+####strings,LISTS
 #"string to letters"
 string = "Geeksforgeeks"
 letter = [x for x in string] 
@@ -11,6 +12,11 @@ print([*string])
 #"length of string"
 a=len(nums) #nums is a List
 
+a.sort()#sorts list a the object itself is changed
+b=sorted(a)#b=a.sort(), a remains unsorted
+
+
+#####DICTIONARIES
 #Check whether given Key already exists in a Python Dictionary
 #if key in dic.keys():
 #if key in dic:
@@ -24,3 +30,33 @@ key_value_pairs = zip(keys, values)
 
 # convert the list of key-value pairs to a dictionary
 my_dict = dict(key_value_pairs)
+
+##Appending to List in Python Dictionary
+dict[sortedStr].append(string)
+#Default Dic
+from collections import defaultdict
+  
+  
+# Function to return a default
+# values for keys that is not
+# present
+def def_value():
+    return "Not Present"
+      
+# Defining the dict
+d = defaultdict(def_value)
+d["a"] = 1
+d["b"] = 2
+  
+print(d["a"])
+print(d["b"])#oytput 2
+print(d["c"]) #output : nor present
+#with defaultdict
+"""def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
+        
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            anagram_map[sorted_word].append(word)
+        
+        return list(anagram_map.values())"""

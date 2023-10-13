f800a5e43bfdfcca43a72ac55afd596e89a72767
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2023-10-13 21:32:06 +0300

    Update useful.py

--- useful.py
+++ useful.py
@@ -66,3 +66,21 @@ print(d["c"]) #output : nor present
             anagram_map[sorted_word].append(word)
         
         return list(anagram_map.values())"""
+
+#leetcode problem
+class Solution:
+    def pancakeSort(self, arr):
+        arr_sorted = sorted(arr, reverse = True)
+
+        k_list = []
+        for maxi in arr_sorted:
+            k = arr.index(maxi) + 1 #max index+1
+            k_list.append(k)
+
+            arr = arr[:k][::-1] + arr[k:] #Αντιστροφή από 0 έως k(άρα, το max είναι στο index 0!)
+            arr = arr[::-1] #Ολικό reverse(άρα κάναμε flip με k = lenght(arr). ΠΡΟΣΟΧΗ, όχι του αρχηκού arr, αλλά αυτού στο οποιό δουλεύουμε τώρα). Το max είναι στο index = -1.
+            k_list.append(len(arr))
+            arr.pop(-1) #Δεν μας νοιάζει πια αυτό το max, άρα δουλεύουμε σε υπολίστα!
+
+        return k_list;
+        
